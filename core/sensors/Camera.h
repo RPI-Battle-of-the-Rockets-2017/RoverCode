5e791fc661b9382b206eebecce5cfda95405afea
Adam Li <84liadam@gmail.com>
2017-10-30 19:38:14 -0400

    Initial Commit

---
+++ core/sensors/Camera.h
@@ -0,0 +1,16 @@
+#ifndef ROVER_CAMERA_H
+#define ROVER_CAMERA_H
+
+/**
+ * Isolate important camera control functions into here
+ */
+
+namespace Rover {
+
+class Camera {
+    Camera();
+};
+
+}
+
+#endif // ROVER_CAMERA_H
---
+++ core/sensors/IMU/Accelerometer.h
@@ -0,0 +1,20 @@
+#ifndef ROVER_IMU_ACCELEROMETER_H
+#define ROVER_IMU_ACCELEROMETER_H
+
+/**
+ * Accelerometer specific functions and whatnot are defined in here
+ */
+
+namespace Rover {
+
+class IMU::Accelerometer {
+    public:
+        //Constructor
+        Accelerometer();
+        //Destructor
+        ~Accelerometer();
+};
+
+}
+
+#endif // ROVER_IMU_ACCELEROMETER_H
