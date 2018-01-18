d0c2e9d4100d4951c2d2838c82792e59ccc2e0b0
Adam Li <84liadam@gmail.com>
2018-01-17 21:56:57 -0500

    Completed the basic part of the task scheduler

--- src/core/utilities/TaskScheduler.h
+++ src/core/utilities/TaskScheduler.h
@@ -1,7 +1,7 @@
 #ifndef ROVER_IMU_TASKSCHEDULER_H
 #define ROVER_IMU_TASKSCHEDULER_H
 
-
+#include "Arduino.h"
 //WIP!
 
 namespace Rover {
@@ -14,34 +14,34 @@ class TaskScheduler {
             void *context;
             void (*func)(void*);
         private:
-            Task(void (*func_)(void*), void * context_) : func(func_), context(context_){}
-            Task(void (*func_)(void*)) : func(func_), context(NULL){}
+            Task(void (*func_)(void*), void * context_ = NULL, uint32_t time_) : func(func_), context(context_), time(time){}
             void runTask(){
                 (*func)(context);
             }
             void addTask(Task* next){
                 nextTask = next;
             }
-            uint32_t time = 0;
             Task* nextTask = NULL;
         };
     private:
-        Task* highPriority;
-        Task* medPriority;
-        Task* lowPriority;
-        uint32_t earliestTime;
-        //Find out what units
-        int timeBuffer = 100;
+        Task* highPriority = NULL;
+        Task* medPriority = NULL;
+        Task* lowPriority = NULL;
+        Task* timePriority = NULL;
+        uint32_t earliestTime = 0;
+        //milliseconds
+        int timeBuffer = 50;
     public:
         typedef enum {
             HIGH_PRIORITY,
             MEDIUM_PRIORITY,
-            LOW_PRIORITY
+            LOW_PRIORITY,
+            TIME_PRIORITY
         } Priority;
 
         TaskScheduler(int8_t timer = -1);
         //boolean addTimerTask();
-        boolean addTask(TaskScheduler::Task* task, Priority priority, uint32_t timeToRun = 0);
+        void addTask(Task* task, Priority priority);
         void runTasks(int timeLimit);
 };
 
